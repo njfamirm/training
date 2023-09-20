c06f28d59a684f0f88a93f54dcfb8469481d6663
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-20 05:39:28 +0330

    refactor(strapi-custom-api): make keyword in reading list search optional

--- strapi-custom-api/src/api/reading-list-search/services/reading-list-search.ts
+++ strapi-custom-api/src/api/reading-list-search/services/reading-list-search.ts
@@ -1,13 +1,14 @@
-async function search(keyword: string) {
+async function search(keyword?: string) {
   try {
-    const entries = await strapi.entityService.findMany('api::reading-list.reading-list', {});
+    const entries = await strapi.entityService.findMany("api::reading-list.reading-list", {});
 
-    const filteredEntries = entries.filter((entry) => {
-      const keywords = entry.keywords
-        .split(',')
-        .map((keyword) => keyword.trim());
-      return keywords.includes(keyword);
-    });
+    let filteredEntries = entries;
+    if (keyword) {
+      filteredEntries = entries.filter((entry) => {
+        const keywords = entry.keyword.split("\n").map((keyword) => keyword.trim());
+        return keywords.includes(keyword);
+      });
+    }
 
     return filteredEntries;
   } catch (err) {
